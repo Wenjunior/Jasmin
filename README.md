9a8c858e19dd2b0c784ffb2beb52d235957bd73d
Wenjunior <wendersonjunior309@gmail.com>
2026-02-21 11:35:17 -0300

    Web server

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-Simple Event-Driven echo server written in Rust.
+Simple Event-Driven web server written in Rust.
 
 ```
+git clone https://github.com/Wenjunior/Jasmin.git
+cd Jasmin
 sudo cargo run
 ```
--- src/main.rs
+++ src/main.rs
@@ -10,22 +10,28 @@ use mio::{
 	}
 };
 use std::{
-	collections::HashMap,
+	fs,
 	io::{
 		Read,
 		Error,
 		Write,
-		ErrorKind
+		ErrorKind,
 	},
 	net::{
 		IpAddr,
 		Ipv4Addr,
 		SocketAddr
-	}
+	},
+	path::{
+		Path,
+		PathBuf
+	},
+	collections::HashMap
 };
 
 const PORT: u16 = 80;
 const CAPACITY: usize = 128;
+const BASE_DIR: &str = "static";
 const REQUEST_SIZE: usize = 1024;
 const SERVER_TOKEN: Token = Token(0);
 
@@ -80,7 +86,9 @@ fn handle_client(clients: &mut HashMap<usize, TcpStream>, client_id: usize, even
 			poll.registry()
 				.reregister(client, Token(client_id), Interest::WRITABLE)?;
 
-			responses.insert(client_id, data);
+			let response = handle_request(data);
+
+			responses.insert(client_id, response);
 
 			return Ok(());
 		}
@@ -100,6 +108,152 @@ fn handle_client(clients: &mut HashMap<usize, TcpStream>, client_id: usize, even
 	}
 
 	Ok(())
+}
+
+fn parse_request(data: Vec<u8>) -> Result<String, Error> {
+	let request = match String::from_utf8(data) {
+		Ok(request) => request,
+		Err(_) => {
+			return Err(Error::new(ErrorKind::InvalidData, ""));
+		}
+	};
+
+	let mut lines = request.lines();
+
+	let first_line = match lines.nth(0) {
+		Some(first_line) => first_line,
+		None => {
+			return Err(Error::new(ErrorKind::InvalidData, ""));
+		}
+	};
+
+	let path = match first_line.split(' ').nth(1) {
+		Some(path) => path.to_string(),
+		None => {
+			return Err(Error::new(ErrorKind::InvalidData, ""));
+		}
+	};
+
+	Ok(path)
+}
+
+fn prevent_directory_transversal(mut path: String) -> Result<PathBuf, Error> {
+	let base_path = Path::new(BASE_DIR)
+		.canonicalize()?;
+
+	path = path.trim_start_matches('/')
+		.to_string();
+
+	if path.is_empty() {
+		path = String::from("index.html");
+	}
+
+	let full_path = base_path.join(path)
+		.canonicalize()?;
+
+	if !full_path.starts_with(base_path) {
+		return Err(Error::new(ErrorKind::PermissionDenied, ""));
+	}
+
+	Ok(full_path)
+}
+
+fn html_boilerplate(title: &str, message: &str) -> Vec<u8> {
+	format!("<!DOCTYPE html>\n<html lang=\"en\">\n\t<head>\n\t\t<title>{}</title>\n\n\t\t<meta charset=\"UTF-8\"/>\n\t\t<meta name=\"robots\" content=\"noindex\"/>\n\t\t<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"/>\n\t\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n\t</head>\n\t<body>\n\t\t<main>\n\t\t\t{}\n\t\t</main>\n\t</body>\n</html>", title, message).as_bytes().to_vec()
+}
+
+fn get_content(full_path: PathBuf, path: String) -> Result<Vec<u8>, Error> {
+	if !full_path.exists() {
+		return Err(Error::new(ErrorKind::NotFound, ""));
+	}
+
+	if full_path.is_file() {
+		let content = fs::read(full_path)?;
+
+		return Ok(content);
+	}
+
+	let title = format!("Listing: {}", path);
+
+	let entries: Vec<String> = fs::read_dir(full_path)?.filter_map(|entry| {
+		let entry = entry.ok()?;
+
+		entry.file_name()
+			.into_string()
+				.ok()
+	}).collect();
+
+	let mut tags = entries.iter()
+		.map(|entry| format!("<a href=\"{}\">{}</a>", format!("{}/{}", path, entry), entry))
+			.collect::<Vec<_>>()
+				.join("\n\t\t\t");
+
+	if tags.is_empty() {
+		tags = String::from("<p>It's empty.</p>");
+	}
+
+	let html = html_boilerplate(&title, &tags);
+
+	Ok(html)
+}
+
+fn build_response(status: &str, headers: HashMap<&str, &str>, mut body: Vec<u8>) -> Vec<u8> {
+	let mut response = Vec::new();
+
+	response.append(&mut "HTTP/1.0 ".as_bytes().to_vec());
+
+	response.append(&mut status.as_bytes().to_vec());
+
+	response.append(&mut  "\r\n".as_bytes().to_vec());
+
+	for header in headers {
+		response.append(&mut format!("{}: {}\r\n", header.0, header.1).as_bytes().to_vec());
+	}
+
+	response.append(&mut "Server: Jasmin\r\n".as_bytes().to_vec());
+
+	response.append(&mut "\r\n".as_bytes().to_vec());
+
+	response.append(&mut body);
+
+	response
+}
+
+fn handle_request(data: Vec<u8>) -> Vec<u8> {
+	let path = match parse_request(data) {
+		Ok(path) => path,
+		Err(_) => {
+			return build_response("400 Bad Request", HashMap::from([("Content-Type", "text/html")]), html_boilerplate("Something wen't wrong", "<h1>Bad Request</h1>"))
+		}
+	};
+
+	let full_path = match prevent_directory_transversal(path.clone()) {
+		Ok(full_path) => full_path,
+		Err(error) => {
+			if permission_denied(&error) {
+				return build_response("403 Forbidden", HashMap::from([("Content-Type", "text/html")]), html_boilerplate("Something wen't wrong", "<h1>Forbidden</h1>"))
+			}
+
+			if not_found(&error) {
+				return build_response("404 Not Found", HashMap::from([("Content-Type", "text/html")]), html_boilerplate("Something wen't wrong", "<h1>Not Found</h1>"))
+			}
+
+			return build_response("500 Internal Server Error", HashMap::from([("Content-Type", "text/html")]), html_boilerplate("Something wen't wrong", "<h1>Internal Server Error</h1>"))
+		}
+	};
+
+	let body = match get_content(full_path, path) {
+		Ok(body) => body,
+		Err(error) => {
+			if not_found(&error) {
+				return build_response("404 Not Found", HashMap::from([("Content-Type", "text/html")]), html_boilerplate("Something wen't wrong", "<h1>Not Found</h1>"))
+			}
+
+			return build_response("500 Internal Server Error", HashMap::from([("Content-Type", "text/html")]), html_boilerplate("Something wen't wrong", "<h1>Internal Server Error</h1>"))
+		}
+	};
+
+	build_response("200 OK", HashMap::new(), body)
 }
 
 fn close_client(clients: &mut HashMap<usize, TcpStream>, client_id: usize, responses: &mut HashMap<usize, Vec<u8>>, poll: &Poll) {
@@ -162,6 +316,14 @@ fn run_server() -> Result<(), Error> {
 			}
 		}
 	}
+}
+
+fn permission_denied(error: &Error) -> bool {
+	error.kind() == ErrorKind::PermissionDenied
+}
+
+fn not_found(error: &Error) -> bool {
+	error.kind() == ErrorKind::NotFound
 }
 
 fn interrupted(error: &Error) -> bool {
